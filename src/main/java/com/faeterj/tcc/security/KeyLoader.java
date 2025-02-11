1c6778c849d25504bf9acc4efe2069cc0e814dfc
erikmello589 <erikmello589@gmail.com>
2025-02-11 16:37:13 -0300

    :recycle: refactor: Refatorando KeyLoader

--- src/main/java/com/faeterj/tcc/security/KeyLoader.java
+++ src/main/java/com/faeterj/tcc/security/KeyLoader.java
@@ -1,26 +1,39 @@
 package com.faeterj.tcc.security;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class KeyLoader {
 
-    public static PublicKey loadPublicKey() throws Exception {
-        byte[] keyBytes = Files.readAllBytes(Paths.get("/etc/secrets/public.pem"));
-        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
-        KeyFactory kf = KeyFactory.getInstance("RSA");
-        return kf.generatePublic(spec);
+    public static RSAPublicKey loadPublicKey() throws Exception {
+        String publicKeyPath = "/etc/secrets/public.pem";  // Caminho do arquivo da chave pública
+        String publicKeyPEM = readKeyFromFile(publicKeyPath);
+        byte[] encoded = Base64.getDecoder().decode(publicKeyPEM.replaceAll("-----BEGIN PUBLIC KEY-----", "").replaceAll("-----END PUBLIC KEY-----", "").replaceAll("\\s", ""));
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return (RSAPublicKey) keyFactory.generatePublic(keySpec);
     }
 
-    public static PrivateKey loadPrivateKey() throws Exception {
-        byte[] keyBytes = Files.readAllBytes(Paths.get("/etc/secrets/private.pem"));
-        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
-        KeyFactory kf = KeyFactory.getInstance("RSA");
-        return kf.generatePrivate(spec);
+    public static RSAPrivateKey loadPrivateKey() throws Exception {
+        String privateKeyPath = "/etc/secrets/private.pem";  // Caminho do arquivo da chave privada
+        String privateKeyPEM = readKeyFromFile(privateKeyPath);
+        byte[] encoded = Base64.getDecoder().decode(privateKeyPEM.replaceAll("-----BEGIN PRIVATE KEY-----", "").replaceAll("-----END PRIVATE KEY-----", "").replaceAll("\\s", ""));
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+    }
+
+    private static String readKeyFromFile(String filePath) throws IOException {
+        FileInputStream fis = new FileInputStream(filePath);
+        byte[] keyBytes = new byte[fis.available()];
+        fis.read(keyBytes);
+        fis.close();
+        return new String(keyBytes);
     }
 }
