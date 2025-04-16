c3885af23f5a7642d44e4135e465f37dcb97bb9f
Erik Mello <erikmello589@gmail.com>
2025-04-16 15:36:45 -0300

    :bricks: ci: Converter String em RSA

--- src/main/java/com/faeterj/saudesafe/security/SecurityConfig.java
+++ src/main/java/com/faeterj/saudesafe/security/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.faeterj.saudesafe.security;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,16 +30,51 @@ import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
+import jakarta.annotation.PostConstruct;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
     @Value("${JWT_PUBLIC_KEY}")
-    private RSAPublicKey publicKey;
+    private String publicKeyPath;
 
     @Value("${JWT_PRIVATE_KEY}")
+    private String privateKeyPath;
+
+    private RSAPublicKey publicKey;
     private RSAPrivateKey privateKey;
+
+    @PostConstruct
+    private void loadKeys() throws Exception {
+        this.publicKey = readPublicKey(publicKeyPath);
+        this.privateKey = readPrivateKey(privateKeyPath);
+    }
+
+    private RSAPublicKey readPublicKey(String filepath) throws Exception {
+        String key = Files.readString(Paths.get(filepath))
+            .replaceAll("-----BEGIN PUBLIC KEY-----", "")
+            .replaceAll("-----END PUBLIC KEY-----", "")
+            .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getDecoder().decode(key);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return (RSAPublicKey) kf.generatePublic(spec);
+    }
+
+    private RSAPrivateKey readPrivateKey(String filepath) throws Exception {
+        String key = Files.readString(Paths.get(filepath))
+            .replaceAll("-----BEGIN PRIVATE KEY-----", "")
+            .replaceAll("-----END PRIVATE KEY-----", "")
+            .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getDecoder().decode(key);
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return (RSAPrivateKey) kf.generatePrivate(spec);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
